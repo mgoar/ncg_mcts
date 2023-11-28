5716397dee2e9665ee3ce900ad95e72016fe0f8d
mgoar <mgoar@pm.me>
2023-11-28 20:08:12 +0100

    Test changed to GE instead of NE for n=20

--- test/test_MCTS.py
+++ test/test_MCTS.py
@@ -49,20 +49,16 @@ class TestMCTS(unittest.TestCase):
         self.assertEqual(len(mcts.tree.get_vertices()), 1)
 
     def test_homs_messegue(self):
-        # n = 20 (single-arm star rooted at each node)
+        # n = 10 (Petersen)
+        n = 10
+        alpha = 2*n/5
+
         dict_own = {0: [1, 5], 1: [2, 6], 4: [0], 5: [7, 8], 2: [3],
                     3: [4, 7], 6: [9], 7: [6], 9: [4, 8], 8: [2]}
         H_temp = nx.DiGraph()
         for key, val in dict_own.items():
             for dst in val:
                 H_temp.add_edge(key, dst)
-
-        n = 20
-        alpha = 2*n/5
-
-        total_n = 10
-        for x in np.arange(total_n):
-            nx.add_star(H_temp, np.hstack((x, total_n+x)))
 
         dg = nx.adjacency_matrix(H_temp)
         ug = nx.adjacency_matrix(H_temp.to_undirected())
@@ -85,13 +81,54 @@ class TestMCTS(unittest.TestCase):
         state_0.set_scores(val)
 
         gt.draw.graph_draw(
-            ncg.network.G, vertex_text=ncg.network.G.vertex_index, output="fig/initial_pet_graph.pdf")
+            ncg.network.G, vertex_text=ncg.network.G.vertex_index, output="fig/initial_pet_10_graph.pdf")
         gt.draw.graph_draw(
-            ncg.network.ownership, vertex_text=ncg.network.ownership.vertex_index, output="fig/initial_pet_graph_own.pdf")
+            ncg.network.ownership, vertex_text=ncg.network.ownership.vertex_index, output="fig/initial_pet_10_graph_own.pdf")
+
+        # Create instance of MCTS
+        k = 10
+        mcts = MCTS.MCTS(state_0, k)
+
+        # Check is NE
+        self.assertEqual(len(mcts.tree.get_vertices()), 1)
+
+        # n = 20 (single-arm star rooted at each node)
+        n = 20
+        alpha = 2*n/5
+
+        # Append star
+        subg_n = 10
+        for x in np.arange(subg_n):
+            nx.add_star(H_temp, np.hstack((x, subg_n+x)))
+
+        dg = nx.adjacency_matrix(H_temp)
+        ug = nx.adjacency_matrix(H_temp.to_undirected())
+
+        graph = Network.Network(ug.todense(), n)
+        graph.set_ownership(gt.Graph(np.array(np.nonzero(dg)).T))
+
+        # Create NCG
+        ncg = NCG.NCG(graph, alpha, True)
+
+        # Create initial state
+        state_0 = State.State(n, 0, ncg)
+
+        # Update scores
+        val = np.zeros(n)
+        for ii in np.arange(n):
+            agent = Agent.Agent(ii)
+            val[ii] = ncg._compute_cost_player_i(agent)
+
+        state_0.set_scores(val)
+
+        gt.draw.graph_draw(
+            ncg.network.G, vertex_text=ncg.network.G.vertex_index, output="fig/initial_pet_20_graph.pdf")
+        gt.draw.graph_draw(
+            ncg.network.ownership, vertex_text=ncg.network.ownership.vertex_index, output="fig/initial_pet_20_graph_own.pdf")
 
         # Create instance of MCTS
         # Assuming Greedy equilibrium
-        k = 5
+        k = 2
         mcts = MCTS.MCTS(state_0, k)
 
         # Check is NE
